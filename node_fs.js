9c08374b7713def0e4274976481b0d6a8560d293
wangyv <1710670484@qq.com>
2018-04-17 21:33:15 +0800

    通过fs 引入其他文件

---
+++ node_fs.js
@@ -0,0 +1,16 @@
+//同步
+// var fs = require('fs');
+// var data = fs.readFileSync('./file.txt','utf-8');
+// console.log(data);
+// console.log('end');
+
+//异步
+var fs = require('fs');
+fs.readFile('./file.txt','utf-8',function(err,data){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+    }
+});
+console.log('end');
